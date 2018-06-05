f0b51d90f7d23798007ca4c9450a1c95f9312020
Alejandro Osorio <alejandroosoriotrujillo@gmail.com>
2018-06-05 17:36:28 -0500

    Modified turret

--- torreta.py
+++ torreta.py
@@ -8,35 +8,44 @@ ANCHO=1000
 pygame.mixer.init(44100, -16, 2, 2048)
 
 
-screensize = pygame.display.Info()
-RESOLUTION = [screensize.current_w, screensize.current_h]
-bglimit = 10
+#screensize = pygame.display.Info()
+#RESOLUTION = [screensize.current_w, screensize.current_h]
+#bglimit = 10
 #Funciones
 
 def recortarTorr(archivo):
     fondo=pygame.image.load(archivo)
     infoFondo=fondo.get_rect()
     matriz=[]
-    spawn=[[0, 78, 87, 36], [0, 78, 87, 36], [178, 78, 87, 36], [267, 78, 87, 36], [356, 34, 87, 79],
+    spawn=[[0, 34, 87, 79], [89, 34, 87, 79], [178, 34, 87, 79], [267, 34, 87, 79], [356, 34, 87, 79],
             [445, 34, 87, 79], [534, 34, 87, 79], [624, 34, 87, 79], [713, 34, 87, 79]]
-    shoot=[0, 194, 104, 79]
-
+    shoot=[17, 195, 87, 79]
+    explode=[[89, 280, 97, 97], [193, 280, 97, 97], [300, 280, 97, 97], [400, 280, 97, 97], [600, 280, 97, 97]]
     spawnS=[]
     shootS=[]
+    explodeS=[]
 
 
-    #Idle R-L
-    for x in range(10):
+    #Spawn L
+    for x in range(9):
         cuadro=fondo.subsurface(spawn[x])
-        #cuadro=pygame.transform.scale(cuadro, (125, 125))
-        #cuadro2=pygame.transform.flip(cuadro, True, False)
-        #cuadro2=pygame.transform.scale(cuadro2, (125, 125))
         spawnS.append(cuadro)
-        #idleL.append(cuadro2)
+
+    #Unspawn L
+    for x in range(9):
+        cuadro=fondo.subsurface(spawn[8-x])
+        spawnS.append(cuadro)
+
+    #Explode L
+    for x in range(5):
+        cuadro=fondo.subsurface(explode[x])
+        explodeS.append(cuadro)
+
+
     cuadro=fondo.subsurface(shoot)
     shootS.append(cuadro)
 
-    return spawnS, shootS
+    return spawnS, shootS, explodeS
 
 
 def recortarBala(archivo):
@@ -48,8 +57,9 @@ def recortarBala(archivo):
     matriz.append(cuadro)
 
     return matriz
-    
-matrizBala=recortarBala('lasers.png')
+
+matrizBala=recortarBala('torreta.png')
+
 #Clases
 class Turret(pygame.sprite.Sprite):
     def __init__(self, matriz):
@@ -57,7 +67,7 @@ class Turret(pygame.sprite.Sprite):
         self.f=matriz
         self.image=self.f[0][0]
         self.rect=self.image.get_rect()
-        self.indice=1
+        self.indice=0
         self.rect.x=50
         self.rect.y=500
         self.accion=0
@@ -65,6 +75,8 @@ class Turret(pygame.sprite.Sprite):
         self._health = 100
         self.shoottimer = 50
         self.shoot = False
+        self.incremento=1
+        self.spawned=False
 
     def getHealth(self):
         return self._health
@@ -74,57 +86,40 @@ class Turret(pygame.sprite.Sprite):
         channel4.play(blast)
 
     def update(self):
-        #Idle R
+        #Idle L
+
         self.shoottimer -= 1
         if self.shoottimer < 0:
             self.shoot = True
             self.shoottimer = random.randrange(20,50)
+        if self.spawned:
+            if self.shoot:
+                self.accion=1
+                #self.rect.x-=17
+            else:
+                #self.rect.x+=17
+                self.accion=0
+                self.indice=8
 
-        if self.shoot:
-            self.accion=2
-        else:
-            self.accion=0
         if self.accion==0:
             self.image = self.f[self.accion][self.indice]
-            self.indice += 1
+            self.indice += self.incremento
 
-            if self.indice > 2:
-                self.indice=0
-        #Idle L
-        if self.accion==1:
-            self.image = self.f[self.accion][self.indice]
-            self.indice += 1
+            if self.indice >=8:
+                self.incremento=0
+                self.indice+=self.incremento
+                self.spawned=True
 
-            if self.indice > 2:
-                self.indice=0
 
         #1
-        #Attack R
-        if self.accion==2:
-            if self.indice <=2:
-                self.image = self.f[self.accion][self.indice]
-                if self.indice==1:
-                    shoot=Bala(matrizBala)
-                self.indice += 1
-            #Es 7 normalmente
-            if self.indice > 2:
-                self.indice=0
-
         #Attack L
-        if self.accion==3:
-            if self.indice <=2:
-                self.image = self.f[self.accion][self.indice]
-                if self.indice==0:
-                    self.rect.x+=85
-                self.indice += 1
-                if self.indice==1:
-                    self.rect.x-=67
-                if self.indice==2:
-                    self.rect.x-=18
+        if self.accion==1:
+            self.indice=0
+            self.image = self.f[self.accion][self.indice]
+            shoot=Bala(matrizBala)
+            self.shoot=False
+            self.shoottimer=50
 
-            #Es 7 normalmente
-            if self.indice > 2:
-                self.indice=0
 
 
 class Bala (pygame.sprite.Sprite):
@@ -158,3 +153,36 @@ class Bala (pygame.sprite.Sprite):
     def update(self):
         self.rect.x+=self.vel_x
             #Mov diagonal
+
+#PRUEBAS
+'''
+if __name__ == '__main__':
+    pygame.init()
+    pantalla=pygame.display.set_mode([ANCHO, ALTO])  #Crea la ventana
+    todos=pygame.sprite.Group()
+    matrizTurret=recortarTorr('torreta.png')
+    tor=Turret(matrizTurret)
+    todos.add(tor)
+
+
+    print 'Funciona'
+    fin=False
+    reloj=pygame.time.Clock()
+
+
+
+    while not fin:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                fin=True
+
+        todos.update()
+        pantalla.fill([0,0,0])
+        todos.draw(pantalla)
+
+
+        pygame.display.flip()
+        reloj.tick(20)
+'''        
